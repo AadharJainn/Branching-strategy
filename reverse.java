34314a4e57779d128d69281521080af49960212e
AadharJainn <88323712+AadharJainn@users.noreply.github.com>
2022-01-06 07:53:40 +0000

    Adding java code

---
+++ reverse.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class reverse
+{ public static void main(String[] args)
+	{ Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a sentence");
+		String str=sc.nextLine();
+		StringBuffer b=new StringBuffer(str);
+		b.reverse();
+		
+	
+	    if(str.equals(b))
+			{
+				System.out.println("String is palindrome");
+				
+			} 
+		else
+		{
+			System.out.println("String is not palindrome");
+		}
+
+}   }
